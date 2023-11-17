51c4120d70c00562b87a7ecbfeabd1be035c737b
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-17 00:58:14 -0300

    cors

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
-const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
 
 const corsOptions = {
   origin: "https://angular-auth-front-login.vercel.app", 
   optionsSuccessStatus: 200, 
 };
-app.use(cors(corsOptions));
+
+app.use(allowCors);
+app.use(bodyParser.json());
 
 app.post("/sign", (req, res) => {
   const email = "alvarobraz83@gmail.com";
