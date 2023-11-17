96f49310fd65cb75d91bc395cb5c52c569901961
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-17 01:15:22 -0300

    cors

--- index.js
+++ index.js
@@ -7,12 +7,27 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
 
 const corsOptions = {
   origin: "https://angular-auth-front-login.vercel.app/", 
   optionsSuccessStatus: 200, 
 };
-app.use(cors(corsOptions));
+app.use(cors(allowCors));
 
 app.post("/sign", (req, res) => {
   const email = "alvarobraz83@gmail.com";
